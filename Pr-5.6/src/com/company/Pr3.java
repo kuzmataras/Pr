f2ee7e6cb43d9fd68d0823de58c68aee1f192a48
kuzmataras <kuzmataras2020@gmail.com>
2022-04-24 22:04:52 +0300

    q

---
+++ Pr-5.6/src/com/company/Pr3.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Pr3 {
+    public static void main(String[] args){
+        double a = 3.14;
+        System.out.println("Res:" + Math.round(a));
+    }
+}
+
--- pr5.6/src/com/company/Main3.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-public class Main3 {
-    public static void main(String[] args) {
-        double a = 5.55523;
-        System.out.print(Math.round(a));
-    }
-}
