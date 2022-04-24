a4155803f82c89b45c0653757beb872aa6929152
kuzmataras <kuzmataras2020@gmail.com>
2022-04-24 22:46:18 +0300

    q

---
+++ Pr-5.6/src/com/company/Pr2.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Pr2 {
+     public static void main(String[] args) {
+        System.out.println("Ведіть двозначне  число:");
+
+
+        int a = 68;
+        int b = a / 10;
+        int c = a % 10;
+
+        System.out.println(b + c);
+    }
+}
