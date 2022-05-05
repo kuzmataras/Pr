13f7d3086201a0c829e021171e5aa82089324b67
kuzmataras <kuzmataras2020@gmail.com>
2022-05-05 11:01:28 +0300

    q

---
+++ Pr-5.16/src/com/company/Pr1.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Pr1 {
+
+    public static void main(String[] args) {
+        System.out.print(Math.round(Math.random() * 40 - 20));
+
+    }
+}
---
+++ Pr-5.16/src/com/company/Pr3.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Pr3 {
+
+    public static void main(String[] args) {
+        double test = Math.random()* 102;
+        System.out.println(test);
+        System.out.println(String.valueOf(test).length() - 1);
+
+    }
+}
